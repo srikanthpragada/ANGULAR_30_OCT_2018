1f82af33547ad80c391a42f81f944368d6866f75
srikanthpragada <srikanthpragada@gmail.com>
2018-11-08 20:01:16 +0530

    8-NOV

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TableComponent } from './table/table.component';
 import { BindingComponent } from './binding/binidng.component';
+import { PipesDemoComponent } from './pipes/pipesdemo.component';
+import { BracketsPipe } from './pipes/brackets.pipe';
+import { FirstComponent } from './icc/first.component';
+import { SecondComponent } from './icc/second.component';
 
 @NgModule({
   declarations: [
-    BindingComponent
+    FirstComponent, SecondComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [BindingComponent]
+  bootstrap: [FirstComponent,SecondComponent]
 })
 export class AppModule { }
